b8246d79899b1f3115e506a936c59076a42406f1
AlexGsilversat <alexisagouveia@gmail.com>
2026-01-13 15:48:12 -0500

    Fix change in Command scheduling

--- src/main/java/frc/team5115/Robot.java
+++ src/main/java/frc/team5115/Robot.java
@@ -3,6 +3,8 @@ package frc.team5115;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Commands;
+
 import java.io.File;
 import org.littletonrobotics.junction.LogFileUtil;
 import org.littletonrobotics.junction.LoggedRobot;
@@ -129,7 +131,7 @@ public class Robot extends LoggedRobot {
 
         // schedule the autonomous command
         if (autonomousCommand != null) {
-            autonomousCommand.schedule();
+            CommandScheduler.getInstance().schedule(autonomousCommand);
         }
         robotContainer.autoInit();
     }
