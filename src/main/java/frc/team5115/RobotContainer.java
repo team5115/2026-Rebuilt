69b478928ed9d39c3c89ca848942cc75ed8f8200
avidcoder27 <gilireeves@gmail.com>
2026-01-17 15:18:32 -0500

    save the successful distances

--- src/main/java/frc/team5115/RobotContainer.java
+++ src/main/java/frc/team5115/RobotContainer.java
@@ -1,7 +1,11 @@
 package frc.team5115;
 
+import static edu.wpi.first.units.Units.Meters;
+
 import com.pathplanner.lib.auto.AutoBuilder;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.util.function.BooleanConsumer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
@@ -17,7 +21,9 @@ import frc.team5115.subsystems.shooter.Shooter;
 import frc.team5115.subsystems.shooter.ShooterIO;
 import frc.team5115.subsystems.shooter.ShooterIOSim;
 import frc.team5115.subsystems.shooter.ShooterIOSparkMax;
+import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
+import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 /**
@@ -39,6 +45,9 @@ public class RobotContainer {
     private final LoggedDashboardChooser<Command> autoChooser;
 
     private final DoubleSupplier speedSupplier;
+    private final BooleanSupplier hitTargetSupplier;
+    private final BooleanConsumer hitTargetConsumer;
+    private final DoubleSupplier distanceMetersSupplier;
 
     // Works with faults
 
@@ -93,9 +102,21 @@ public class RobotContainer {
                 break;
         }
 
-        final String key = "ShooterSpeedInput";
-        SmartDashboard.putNumber(key, 0);
-        speedSupplier = () -> SmartDashboard.getNumber(key, 0);
+        final String speedKey = "ShooterSpeedInput";
+        final String targetKey = "HitTarget?";
+        final String feetKey = "Feet Distance";
+        final String inchKey = "Inch Distance";
+        SmartDashboard.putNumber(speedKey, 0);
+        SmartDashboard.putBoolean(targetKey, false);
+        SmartDashboard.putNumber(feetKey, 0);
+        SmartDashboard.putNumber(inchKey, 0);
+        speedSupplier = () -> SmartDashboard.getNumber(speedKey, 0);
+        hitTargetSupplier = () -> SmartDashboard.getBoolean(targetKey, false);
+        hitTargetConsumer = (v) -> SmartDashboard.putBoolean(targetKey, v);
+        distanceMetersSupplier =
+                () ->
+                        Units.feetToMeters(SmartDashboard.getNumber(feetKey, 0))
+                                + Units.inchesToMeters(SmartDashboard.getNumber(inchKey, 0));
 
         autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
 
@@ -117,7 +138,13 @@ public class RobotContainer {
         driverController.configureRumbleBindings(drivetrain);
     }
 
-    public void robotPeriodic() {}
+    public void robotPeriodic() {
+        if (hitTargetSupplier.getAsBoolean()) {
+            Logger.recordOutput(
+                    "ShooterData/SuccessfulDistance", Meters.of(distanceMetersSupplier.getAsDouble()));
+            hitTargetConsumer.accept(false);
+        }
+    }
 
     /**
      * Use this to pass the autonomous command to the main {@link Robot} class.
