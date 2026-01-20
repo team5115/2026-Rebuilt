82a34703cea3900b9390f2ee72de37e21cdf9a34
avidcoder27 <gilireeves@gmail.com>
2026-01-20 15:14:19 -0500

    murdered rev log

--- src/main/java/frc/team5115/Robot.java
+++ src/main/java/frc/team5115/Robot.java
@@ -1,5 +1,6 @@
 package frc.team5115;
 
+import com.revrobotics.util.StatusLogger;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -27,6 +28,8 @@ public class Robot extends LoggedRobot {
      */
     @Override
     public void robotInit() {
+        // Disables rev log because it logs into the rio
+        StatusLogger.disableAutoLogging();
         // Record metadata
         Logger.recordMetadata("ProjectName", BuildConstants.MAVEN_NAME);
         Logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE);
