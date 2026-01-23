974f6611d000138d366b0efc3baae5010af974de
avidcoder27 <gilireeves@gmail.com>
2026-01-23 14:58:11 -0500

    disable rev logging

--- src/main/java/frc/team5115/Robot.java
+++ src/main/java/frc/team5115/Robot.java
@@ -10,6 +10,8 @@ import org.littletonrobotics.junction.networktables.NT4Publisher;
 import org.littletonrobotics.junction.wpilog.WPILOGReader;
 import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 
+import com.revrobotics.util.StatusLogger;
+
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
@@ -26,6 +28,7 @@ public class Robot extends LoggedRobot {
      */
     @Override
     public void robotInit() {
+        StatusLogger.disableAutoLogging();
         // Record metadata
         Logger.recordMetadata("ProjectName", BuildConstants.MAVEN_NAME);
         Logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE);
