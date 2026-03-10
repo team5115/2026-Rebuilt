765aee0495fa18a66726d5199c875a4b880d9dc2
AvidCoder27 <gilireeves@gmail.com>
2026-03-10 18:04:22 -0400

    Revert "shooter accel tracking"
    This reverts commit 579778faf7b9e22407fc78d4a38ac924e64063b7.

--- src/main/java/frc/team5115/subsystems/shooter/Shooter.java
+++ src/main/java/frc/team5115/subsystems/shooter/Shooter.java
@@ -53,8 +53,6 @@ public class Shooter extends SubsystemBase implements MotorContainer {
     /** Prevents the PID from saying its at setpoint when a new setpoint has just been requested */
     @AutoLogOutput private boolean newSetpoint = false;
 
-    @AutoLogOutput private double previousVelocity;
-
     public Shooter(ShooterIO io, DoubleSupplier distanceToHub) {
         this.io = io;
         this.distanceToHub = distanceToHub;
@@ -104,7 +102,6 @@ public class Shooter extends SubsystemBase implements MotorContainer {
 
     @Override
     public void periodic() {
-        previousVelocity = inputs.velocityRPM;
         io.updateInputs(inputs);
         Logger.processInputs("Shooter", inputs);
         Logger.recordOutput("Info/DistanceToHub", distanceToHub);
@@ -250,14 +247,9 @@ public class Shooter extends SubsystemBase implements MotorContainer {
 
     @AutoLogOutput
     public boolean isJammed() {
-        final double derivative =
-                Math.abs(inputs.velocityRPM - previousVelocity) / Constants.LOOP_PERIOD_SECS;
-        Logger.recordOutput("Shooter/AccelerationRPMPerSecond", derivative);
-        final boolean isJamming = derivative < 1000 && derivative > 10;
-        return false
-                && isJamming
-                && !atSetpoint()
-                && currentlyRequested(); // TODO tune tolerance for jam
+        final double derivative = Math.abs(pid.getErrorDerivative());
+        final boolean isJamming = derivative < 100 && derivative > 10;
+        return isJamming && !atSetpoint() && currentlyRequested(); // TODO tune tolerance for jam
     }
 
     private Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
