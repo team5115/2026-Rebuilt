a8df19c1319297f7d494ddaeddb2dc74ed3f587d
avidcoder27 <gilireeves@gmail.com>
2026-09-09 15:33:21 -0400

    agitator motor currents update

--- src/main/java/frc/team5115/subsystems/agitator/AgitatorIOSparkMax.java
+++ src/main/java/frc/team5115/subsystems/agitator/AgitatorIOSparkMax.java
@@ -21,7 +21,7 @@ public class AgitatorIOSparkMax implements AgitatorIO {
 
         final SparkMaxConfig motorConfig = new SparkMaxConfig();
         // Agitator motor configs
-        motorConfig.idleMode(IdleMode.kCoast).smartCurrentLimit(35, 35);
+        motorConfig.idleMode(IdleMode.kCoast).smartCurrentLimit(25, 25);
 
         motor.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     }
